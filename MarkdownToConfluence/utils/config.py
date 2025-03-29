f770ebd17171178710e19d56e36f2637df83a602
jvirgo <jvirgo@artisight.com>
2025-03-29 11:30:01 -0600

    fix(DOP-98): fixing syntax issue

--- MarkdownToConfluence/utils/config.py
+++ MarkdownToConfluence/utils/config.py
@@ -7,6 +7,6 @@ def get_config():
         "AUTH_API_TOKEN": os.environ.get("INPUT_AUTH_API_TOKEN"),
         "SPACE_KEY": os.environ.get("INPUT_CONFLUENCE_SPACE_KEY"),
         "FILES_PATH": os.environ.get("INPUT_FILESLOCATION", "docs"),
-        "PARENT_ID": os.environ.get("INPUT_PARENT_ID") or None
+        "PARENT_ID": os.environ.get("INPUT_PARENT_ID") or None,
         "SHOULD_UPLOAD": os.environ.get("INPUT_SHOULD_UPLOAD") == "true"
     }
